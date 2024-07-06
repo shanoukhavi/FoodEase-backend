a25c777a6e9d5d7c47d7e95f246126b57e18785e
shanoukhavi <shanoukh5@gmail.com>
2024-07-06 20:28:38 +0530

    done

--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -2,10 +2,18 @@ import express from "express";
 import { param } from "express-validator";
 import RestaurantController from "../controllers/RestaurantController";
 
-const router=express.Router();
-//api/restaurant/search/bajpe
-router.get("/search/:city",param("city").isString().trim().notEmpty().withMessage("City paramater must be a valid String"),
-RestaurantController.searchRestaurant);
-//whenever eh clicks go for controller fn  forwardd onto the restaurantcontroller of search restaurants 
-//whatever city orrr ee country if he searches he will ge tit latetr 
+const router = express.Router();
+
+router.get(
+    "/:restaurantId",
+    param("restaurantId").isString().trim().notEmpty().withMessage("Restaurant Id parameter must be a valid string"),
+    RestaurantController.getRestaurant
+);
+
+router.get(
+    "/search/:city",
+    param("city").isString().trim().notEmpty().withMessage("City parameter must be a valid string"),
+    RestaurantController.searchRestaurant
+);
+
 export default router;
